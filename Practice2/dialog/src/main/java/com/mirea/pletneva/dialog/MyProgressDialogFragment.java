7042305d7c7a6ad04d640ba7dfbf363abc81c2ad
KelionRedfox <yamariya999@gmail.com>
2021-06-06 16:44:23 +0300

    practice 2 done
    zero exceptions VI KA

---
+++ Practice2/dialog/src/main/java/com/mirea/pletneva/dialog/MyProgressDialogFragment.java
@@ -0,0 +1,10 @@
+package com.mirea.pletneva.dialog;
+
+import android.app.ProgressDialog;
+import android.content.Context;
+
+public class MyProgressDialogFragment extends ProgressDialog {
+    public MyProgressDialogFragment(Context context) {
+        super(context);
+    }
+}
